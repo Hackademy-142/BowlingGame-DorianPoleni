744320ffc924834c8a4c3dc81de2fb27539f48f7
Dorian Poleni <dpoleni@yahoo.it>
2024-03-27 09:17:13 +0100

    modified table creation

--- main.js
+++ main.js
@@ -14,6 +14,41 @@ const bowling = {
     //Current players
     "players" : [],
     
+    //Create New Player
+    "createPlayer" : function(playerName) {
+        let newPlayerName = playerName;
+        this.players.push( {"name" : newPlayerName , "scores" : [] , "totalScore" : 0} );
+        console.log(this.players);
+    },
+
+        //Create Table
+    "createTable" : function() {
+        this.players.forEach( (player,i) => {
+            let playerRow = document.createElement("tr");
+
+            tabPlayerscontainer.innerHTML = ""
+            // tabPlayerscontainer.appendChild(playerRow);
+
+            playerRow.innerHTML = `
+            <th scope="row">${i + 1}</th>                            
+            <td>${player.name}</td>                            
+            <td>${player.scores[0] ? player.scores[0] : 0}</td>
+            <td>${player.scores[1] ? player.scores[1] : 0}</td>
+            <td>${player.scores[2] ? player.scores[2] : 0}</td>
+            <td>${player.scores[3] ? player.scores[3] : 0}</td>
+            <td>${player.scores[4] ? player.scores[4] : 0}</td>
+            <td>${player.scores[5] ? player.scores[5] : 0}</td>
+            <td>${player.scores[6] ? player.scores[6] : 0}</td>
+            <td>${player.scores[7] ? player.scores[7] : 0}</td>
+            <td>${player.scores[8] ? player.scores[8] : 0}</td>
+            <td>${player.scores[9] ? player.scores[9] : 0}</td>
+            <td>0</td>
+            `
+            tabPlayerscontainer.appendChild(playerRow);
+            
+        }    )
+    },
+
     //Set players score randomly
     "setScore" : function() {
         this.players.forEach( (nPlayer) => {
@@ -66,62 +101,38 @@ const bowling = {
 
 //Add new player
 btnAddPlayer.addEventListener("click", () => {
-    let newPlayerName = playerName.value;
-    bowling.players.push( {"name" : newPlayerName , "scores" : [] , "totalScore" : 0} );
-    console.log(bowling.players);
+
+    bowling.createPlayer(playerName.value);
     playerName.value = "";
-    
-    let tr = document.createElement("tr");
-    tr.innerHTML = 
-    `<tr id="tableRow">
-    <th scope="row">${newPlayerName}</th>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    <td>0</td>
-    </tr>`
-    tabPlayerscontainer.appendChild(tr);    
+     
 })
 
+let roundCounter = 0;
 
 //Play a round
 startGame.addEventListener("click", () => {
-    bowling.players.forEach( (nPlayer) => {
-        for (let i = 0; i < 10; i++) {
-            nPlayer.scores.push(random1_10());
-        }
+    bowling.createTable();
+    
+    
+    
+    
+    
+    
+    // bowling.players.forEach( (nPlayer) => {       
+    //     if (roundCounter < 10){
+    //         nPlayer.scores.push(random1_10()) ;
+    //         let th = document.createElement("th");
+    //         th.innerHTML = 
+    //         `<td>${nPlayer.scores[roundCounter]}</td>`
+    //         tabPlayerscontainer.appendChild(th);   
+    //         roundCounter ++;
+    //     }
         
-        let tr = document.createElement("tr");
-        tr.innerHTML = 
-        `<tr id="tableRow">
-        <th scope="row">${newPlayerName}</th>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        <td>0</td>
-        </tr>`
-        tabPlayerscontainer.appendChild(tr);    
+    //     console.log(nPlayer);
+    //     console.log(roundCounter);
         
-        
-        
-        
-        
-    }
-    )
+    // }
+    // )
 })
 
 
